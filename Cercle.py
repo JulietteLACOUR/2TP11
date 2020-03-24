d0cdc210265be18fc00b8c4dcee2d662956cacd4
juliette lacour <juliette.lacour@isen-ouest.yncrea.fr>
2020-03-24 11:16:16 +0100

    TP11 2020

---
+++ Cercle.py
@@ -0,0 +1,31 @@
+class Cercle:
+    def __init__(self, rayon):
+        self.__R = rayon
+
+    def getR(self):
+        return self.__R
+
+    def __add__(self, C):
+       return Cercle(self.__R + C.getR())
+
+    def __lt__(self, C):
+       return self.__R < C.getR()
+
+    def __gt__(self, C):
+       return self.__R > C.getR()
+
+    def __str__(self):
+      return "Rayon du cercle : R="+str(self.__R)
+
+
+if __name__== '__main__':
+   c1 = Cercle(2)
+   c2 = Cercle(3)
+   c3 = c1 + c2
+   c4 = c1 < c2
+   c5 = c2 > c3
+   print(c1)
+   print(c2)
+   print(c3)
+   print(c4)
+   print(c5)
